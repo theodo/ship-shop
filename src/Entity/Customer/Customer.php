4e708a46645497755aa2fbe8a87dea646dc1b378
Max-Eugène Mamet <max-eugene@hotmail.fr>
2023-02-01 16:12:08 +0100

    trying to make CI fail

--- src/Entity/Customer/Customer.php
+++ src/Entity/Customer/Customer.php
@@ -13,4 +13,33 @@ use Sylius\Component\Core\Model\Customer as BaseCustomer;
  */
 class Customer extends BaseCustomer
 {
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private ?string $firstName = null;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private ?string $lastName = null;
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(?string $firstName): void
+    {
+        $this->firstName = $firstName;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(?string $lastName): void
+    {
+        $this->lastName = $lastName;
+    }
 }
